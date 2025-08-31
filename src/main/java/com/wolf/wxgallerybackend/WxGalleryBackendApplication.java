16080cbefe239515647d35cb6333be848c0f54e6
wx <547036318@qq.com>
2025-08-31 15:33:25 +0800

    add package and setting: mybatis-plus

--- src/main/java/com/wolf/wxgallerybackend/WxGalleryBackendApplication.java
+++ src/main/java/com/wolf/wxgallerybackend/WxGalleryBackendApplication.java
@@ -1,9 +1,11 @@
 package com.wolf.wxgallerybackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wolf.wxgallerybackend.mapper")
 public class WxGalleryBackendApplication {
 
     public static void main(String[] args) {
