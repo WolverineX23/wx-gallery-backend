9f0d7a8aa29a34deea5838fb48260a528ef1f35f
wx <547036318@qq.com>
2025-08-31 15:08:12 +0800

    init project

---
+++ src/main/java/com/wolf/wxgallerybackend/WxGalleryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wolf.wxgallerybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxGalleryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxGalleryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wolf/wxgallerybackend/WxGalleryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolf.wxgallerybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxGalleryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
