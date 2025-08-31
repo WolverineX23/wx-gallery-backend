42cfa4df13762f29561f0a52eba5bdafb80953fd
wx <547036318@qq.com>
2025-08-31 15:55:50 +0800

    add package: aop

--- src/main/java/com/wolf/wxgallerybackend/WxGalleryBackendApplication.java
+++ src/main/java/com/wolf/wxgallerybackend/WxGalleryBackendApplication.java
@@ -3,9 +3,11 @@ package com.wolf.wxgallerybackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.wolf.wxgallerybackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true) // 开启 AOP 代理
 public class WxGalleryBackendApplication {
 
     public static void main(String[] args) {
